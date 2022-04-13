8a8cc5be75ac5edbb369a17c67027d310bfd5680
Mahas1 <mahasvan@gmail.com>
2022-04-13 18:08:36 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import json
+import os
+import shutil
+import subprocess
+import threading
+
+import re
+
+import spotify
+import misc
+tracks_details = []
+
+playlist_verifier_regex = r"^https:\/\/open\.spotify\.com\/playlist\/(.)+?(\?si=)+?"
+playlist_id_regex = r"(?<=\/playlist\/)(.*?)(?=\?|$)"
+playlist_url = input("Enter playlist url: ")
+if not re.match(playlist_verifier_regex, playlist_url):
+    print("Invalid playlist url")
+    playlist_id = ""
+    exit()
+else:
+    playlist_id = re.search(playlist_id_regex, playlist_url).group(0)
+
+playlist_tracks = spotify.playlist_track_details(playlist_id)
+for i in playlist_tracks:
+    tracks_details.append(spotify.get_track_metadata(i))
+
+to_download = {track["song_name"]: track["song_url"] for track in tracks_details}
+failed = []
+succeeded = []
+
+max_threads = 10
+lock = threading.Lock()
+threads_to_run = []
+
+shutil.rmtree("spotdl-dls", ignore_errors=True)
+
+try:
+    os.mkdir("spotdl-dls")
+except FileExistsError:
+    pass
+
+os.chdir("spotdl-dls")  # change directory to spotdl-dls to download the files over there
+
+
+def download_song(song_name, song_url):
+    print(f"Downloading: {song_name} with URL: {song_url}")
+    term_command = subprocess.run(["spotdl", song_url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    success = True
+    if term_command.returncode == 0:
+        print(f"Downloaded: {song_name}")
+    else:
+        print(f"Failed to download: {song_name}")
+        success = False
+    lock.acquire()
+    if success:
+        succeeded.append(song_url)
+    else:
+        failed.append(song_url)
+    with open("../succeeded.json", "w") as f:
+        json.dump(succeeded, f, indent=4)
+    with open("../failed.json", "w") as f:
+        json.dump(failed, f, indent=4)
+    lock.release()
+
+
+for track in tracks_details:
+    threads_to_run.append(
+        threading.Thread(target=download_song, args=(track["song_name"], track["song_url"])))
+
+split_threads = misc.chunks(threads_to_run, max_threads)
+
+for thread_chunk in split_threads:
+    for thread in thread_chunk:
+        thread.start()
+    for thread in thread_chunk:
+        thread.join()
+
+os.chdir("..")  # change back to root directory
+print("Done!")
+print(f"Succeeded: {len(succeeded)}")
+print(f"Failed: {len(failed)}")
---
+++ spotify.py
@@ -0,0 +1,54 @@
+import json
+import subprocess
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+scope = "user-library-read,user-library-modify,playlist-modify-private," \
+        "playlist-read-private,playlist-modify-public,playlist-read-collaborative"
+with open('config.json') as config_file:
+    config = json.load(config_file)
+client_id = config.get('client_id')
+if not client_id:
+    client_id = input("Enter your client id: ")
+client_secret = config.get('client_secret')
+if not client_secret:
+    client_secret = input("Enter your client secret: ")
+
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+
+def playlist_track_details(playlist_id):
+    results = spotify.playlist_items(playlist_id, limit=100)
+    tracks = results["items"]
+    while results["next"]:
+        results = spotify.next(results)
+        tracks.extend(results["items"])
+    return [x["track"] for x in tracks]
+
+
+def get_track_metadata(track):
+    return {
+        "song_name": track["name"],
+        "song_album_name": track["album"]["name"],
+        "song_artists": [artist["name"] for artist in track["artists"]],
+        "song_duration": round(track["duration_ms"] / 1000, ndigits=3),
+        "isrc": track.get('external_ids', {}).get('isrc'),
+        "song_id": track["id"],
+        "song_url": track["external_urls"]["spotify"],
+    }
+
+
+def playlist_tracks(playlist_id):
+    tracks = []
+    results = spotify.playlist_items(playlist_id, limit=100)
+    while results["next"]:
+        results = spotify.next(results)
+        tracks.extend(results["items"])
+    return tracks
+
+
+def get_track_details(track_id):
+    track = spotify.track(track_id)
+    return track
+
